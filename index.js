b601b7c9ab2f455f4edb28dd6bc5bd465db77cec
IAmToufiqurRahman <toufiqurrahmanaust@gmail.com>
2021-07-31 21:55:42 +0600

    adding farm for products to be associated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 
 const Product = require('./models/product')
+const Farm = require('./models/farm')
 const methorOverride = require('method-override')
 
 mongoose
@@ -25,6 +26,51 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true })) // This middleware is used to get the request body
 app.use(methorOverride('_method')) // Method override is used to access patch/put requests in form because forms only have get & post request
 
+// Farm routes
+app.get('/farms', async (req, res) => {
+  const farms = await Farm.find({})
+  res.render('farms/index', { farms })
+})
+
+app.get('/farms/new', (req, res) => {
+  res.render('farms/new')
+})
+
+app.post('/farms', async (req, res) => {
+  const farm = new Farm(req.body)
+  await farm.save()
+  res.redirect('/farms')
+})
+
+app.get('/farms/:id', async (req, res) => {
+  const farm = await Farm.findById(req.params.id).populate('products')
+  res.render('farms/show', { farm })
+})
+
+app.get('/farms/:id/products/new', async (req, res) => {
+  const { id } = req.params
+  const farm = await Farm.findById(id)
+  res.render('products/new', { categories, farm })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+  const { id } = req.params
+  const farm = await Farm.findById(id)
+  const { name, price, category } = req.body
+  const product = new Product({ name, price, category })
+  farm.products.push(product)
+  product.farm = farm
+  await farm.save()
+  await product.save()
+  res.redirect(`/farms/${id}`)
+})
+
+app.delete('/farms/:id', async (req, res) => {
+  const farm = await Farm.findByIdAndDelete(req.params.id)
+  res.redirect('/farms')
+})
+
+// Product routes
 const categories = ['fruit', 'vegetables', 'dairy']
 
 app.get('/products', async (req, res) => {
@@ -53,7 +99,7 @@ app.post('/products', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
   const { id } = req.params
-  const product = await Product.findById(id)
+  const product = await Product.findById(id).populate('farm', 'name')
 
   res.render('products/show', { product })
 })
